8e7502c4296995175e382afc94d184099dcdbc85
Santiago Sica <santisica29@gmail.com>
2026-01-11 23:54:05 -0300

    Add project files.

---
+++ TodoListMAUI/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TodoListMAUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TodoListMAUI.Pages")]
